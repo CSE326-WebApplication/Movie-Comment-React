a4c3dd3a2ca901cdfff9175b4f9e6ad231593497
nayunhwan <nayunhwan.dev@gmail.com>
2017-09-22 22:01:32 +0900

    fix(webpack): add Fileloader for images

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,10 @@ module.exports = {
         test: /\.scss$/,
         loaders: ['style-loader', 'css-loader', 'sass-loader'],
         exclude: /node_modules/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: "file-loader?name=/public/icons/[name].[ext]"
       }
     ]
   }
