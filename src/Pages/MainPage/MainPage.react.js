e7121e30f3b2c2d31578b47834554101de7e790b
nayunhwan <nayunhwan.dev@gmail.com>
2017-09-25 01:38:53 +0900

    feat(MainPage): add Searchbar

--- src/Pages/MainPage/MainPage.react.js
+++ src/Pages/MainPage/MainPage.react.js
@@ -4,10 +4,13 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { MainSlider, Navigation } from '../../Components';
+import { Search, Loader } from 'semantic-ui-react'
+
+import { MainSlider, Navigation, SearchResultRenderer } from '../../Components';
 
 import * as MovieActionCreator from '../../ActionCreators/MovieActionCreator';
 import * as NaverMovieActionCreator from '../../ActionCreators/NaverMovieActionCreator';
+import * as TMDBActionCreator from '../../ActionCreators/TMDBActionCreator';
 
 const defaultProps = {};
 const propTypes = {};
@@ -16,35 +19,97 @@ const mapStateToProps = state => {
 	return {
 		value: state.movieReducer.text,
 		boxoffices: state.movieReducer.boxoffices,
+		searchedMovies: state.movieReducer.searchedMovies,
+		searchedList: state.movieReducer.searchedList,
 	};
 };
 
 class MainPage extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			selected: null,
+		}
 	}
 
 	componentDidMount() {
-		this.props.dispatch(MovieActionCreator.getBoxoffices());
-		this.props.dispatch(NaverMovieActionCreator.getMovieInformation('it'));
+		// this.props.dispatch(MovieActionCreator.getBoxoffices());
+		// this.props.dispatch(NaverMovieActionCreator.getMovieInformation('it'));
 	}
-	
-	render() {
+
+	componentWillReceiveProps(nextProps) {
 		const { boxoffices } = this.props;
+
+		if (boxoffices !== nextProps.boxoffices) {
+			const movieNm = nextProps.boxoffices.boxOfficeResult.dailyBoxOfficeList[0].movieNm;
+			this.props.dispatch(NaverMovieActionCreator.getMovieInformation(movieNm));
+		}
+	}
+
+	resetComponent() {
+		this.setState({
+			searchedMovies: null,
+		});
+	}
+
+	handleResultSelect(e, { result }) {
+		this.setState({
+			value: result.title,
+			backdrop: result.backdrop_path,
+			selected: result,
+		});
+	}
+
+	handleSearchChange(e, { value }) {
+		if (this.timer) {
+			clearTimeout(this.timer);
+		}
+
+		this.setState({ isLoading: true, value }, () => {
+			this.timer = setTimeout(() => {
+				if (this.state.value.length < 1) return this.resetComponent();
+				this.props.dispatch(TMDBActionCreator.getSearchedList(this.state.value));
+			}, 500);
+		});
+	}
+
+	render() {
+		const { boxoffices, searchedList } = this.props;
 		return (
-			<div>
-				<Navigation/>
-				<MainSlider/>
+			<div className="mainPage">
+				<Navigation>
+					<Search
+						className="navigation__body__search"
+						icon="search"
+						size="huge"
+						onResultSelect={(e, result) => this.handleResultSelect(e, result)}
+						onSearchChange={(e, value) => this.handleSearchChange(e, value)}
+						value={this.state.value}
+						placeholder="영화, 감독, 출연진 검색..."
+						results={searchedList}
+						resultRenderer={item => (<SearchResultRenderer item={item}/>)}
+					/>
+				</Navigation>
+				<MainSlider
+					backdrop={this.state.backdrop}
+					item={this.state.selected}
+				/>
 				<ol>
 					{
-						boxoffices && boxoffices.boxOfficeResult.dailyBoxOfficeList.map((item, i) => {
-							console.log(item);
+						boxoffices !== null ? boxoffices.boxOfficeResult.dailyBoxOfficeList.map((item, i) => {
 							return (
 								<li key={i}>{item.movieNm}</li>
 							);
 						})
+						:(<Loader inverted>Loading</Loader>)
 					}
 				</ol>
+				{
+					this.props.information && (<img src={this.props.information.items[0].image}/>)
+				}
+				<Loader inverted>Loading</Loader>
+				<Loader>Loading</Loader>
 			</div>
 		);
 	}
