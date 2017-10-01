43648f0bedddfb0a480e2120b5e84fd88f90aa7f
nayunhwan <nayunhwan.dev@gmail.com>
2017-10-01 16:46:03 +0900

    feat(app.js): check 'islogged in' when it is connected

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@
 import React, { Component } from 'react';
 // React Router
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { connect } from 'react-redux';
 
 // Own Modules
 import { LoginPage, MainPage } from './Pages/';
 
+import * as AuthActionCreator from './ActionCreators/AuthActionCreator';
+
+const mapStateToProps = () => {
+	return {};
+};
 
 class App extends Component {
 
 	constructor(props) {
 		super(props);
+	}
+
+	componentWillMount() {
+		const token = localStorage.getItem('token');
+		if (token) {
+			this.props.dispatch(AuthActionCreator.auth(token));
+		}
 	}
 
 	render() {
@@ -27,4 +40,4 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default App = connect(mapStateToProps)(App);
