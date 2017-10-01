9343afec652557e150f482d3e19e7e4f4df65df5
nayunhwan <nayunhwan.dev@gmail.com>
2017-10-01 16:39:01 +0900

    feat(WebRequestUtil): add headers parameter for get request

--- src/Utils/WebRequestUtil.js
+++ src/Utils/WebRequestUtil.js
@@ -6,9 +6,9 @@
 import fetch from 'isomorphic-fetch';
 import { ServerEndPoint } from '../Configs/Server';
 
-export const getJson = (url, responseActionCreator) => {
+export const getJson = (url, responseActionCreator, headers) => {
 	return dispatch => {
-		fetch(ServerEndPoint + url)
+		fetch(ServerEndPoint + url, { headers })
 		.then(response => response.json())
 		.then(json => dispatch(responseActionCreator(json)));
 	};
