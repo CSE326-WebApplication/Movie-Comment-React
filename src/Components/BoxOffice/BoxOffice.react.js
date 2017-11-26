1c29ec25b99b8a9b6cf4bf2b675b757b76a03490
nayunhwan <nayunhwan.dev@gmail.com>
2017-11-26 20:08:49 +0900

    fix(BoxOffice): split movie list every 3n

--- src/Components/BoxOffice/BoxOffice.react.js
+++ src/Components/BoxOffice/BoxOffice.react.js
@@ -15,7 +15,23 @@ class BoxOffice extends Component {
 		return (
 			<div className="boxOffice__posters">
 				{
-					boxOffices.results.slice(0, 6).map((movie, i) => {
+					boxOffices.results.slice(0, 3).map((movie, i) => {
+						return (
+							<div
+								className="boxOffice__posters__item"
+								key={i}
+							>
+								<img
+									className="boxOffice__posters__item__posterImg"
+									src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`}
+								/>
+							</div>
+						);
+					})
+				}
+				<br />
+				{
+					boxOffices.results.slice(3, 6).map((movie, i) => {
 						return (
 							<div
 								className="boxOffice__posters__item"
