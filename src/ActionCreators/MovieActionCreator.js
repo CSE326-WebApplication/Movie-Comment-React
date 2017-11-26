0513cd1705b806ea9e7033854d293b9692008838
nayunhwan <nayunhwan.dev@gmail.com>
2017-11-26 17:03:09 +0900

    fix(MovieActionCreator): disable MovieActionCreator

--- src/ActionCreators/MovieActionCreator.js
+++ src/ActionCreators/MovieActionCreator.js
@@ -4,21 +4,21 @@
 */
 
 import * as WebRequestUtil from '../Utils/WebRequestUtil';
-
-export const GET_BOXOFFICES = 'GET_BOXOFFICES';
-
-export const getBoxoffices = () => {
-	return getBoxoffices_request();
-};
-
-const getBoxoffices_request = () => {
-	const url = 'api/openMovie';
-	return WebRequestUtil.getJson(url, getBoxoffices_response);
-};
-
-const getBoxoffices_response = (json) => {
-	return {
-		type: GET_BOXOFFICES,
-		boxoffices: json,
-	};
-};
+// 
+// export const GET_BOXOFFICES = 'GET_BOXOFFICES';
+//
+// export const getBoxoffices = () => {
+// 	return getBoxoffices_request();
+// };
+//
+// const getBoxoffices_request = () => {
+// 	const url = 'api/openMovie';
+// 	return WebRequestUtil.getJson(url, getBoxoffices_response);
+// };
+//
+// const getBoxoffices_response = (json) => {
+// 	return {
+// 		type: GET_BOXOFFICES,
+// 		boxoffices: json,
+// 	};
+// };
