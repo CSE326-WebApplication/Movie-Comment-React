5e3846cd5b8e23b986d10cb7768d8b62b8a382a9
nayunhwan <nayunhwan.dev@gmail.com>
2017-11-27 16:36:34 +0900

    feat(MovieViewer): create MovieViewer as a new Component

--- src/Components/index.js
+++ src/Components/index.js
@@ -8,5 +8,6 @@ export DefaultComponent from './_DefaultComponent/DefaultComponent.react';
 export BoxOffice from './BoxOffice/BoxOffice.react';
 export Footer from './Footer/Footer.react';
 export MainSlider from './MainSlider/MainSlider.react';
+export MovieViewer from './MovieViewer/MovieViewer.react';
 export Navigation from './Navigation/Navigation.react';
 export SearchResultRenderer from './SearchResultRenderer/SearchResultRenderer.react';
